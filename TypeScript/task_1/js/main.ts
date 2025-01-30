c032d76ee7962b6c20d924eb149fe6c7f1126e5d
NadegeL <nadege.luthier@gmai.com>
2025-01-30 22:02:24 +0100

    fix eslint au passage

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,7 +7,7 @@ interface Teacher {
     location: string;
 
     yearsOfExperience?: number;
-    [key: string]: any;
+    [key: string]: string | number | boolean;
 }
 const teacher1: Teacher = {
     firstName: 'John',
@@ -26,15 +26,42 @@ const director1: Directors = {
     location: 'London',
     fullTimeEmployee: true,
     numberOfReports: 17,
-  };
+};
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
-const printTeacher: printTeacherFunction = (firstName, lastName)=> {
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
 
 console.log(teacher1);
 console.log(director1);
 console.log(printTeacher('John', 'Doe'));
 console.log("The scrit of Typescript is running !");
+console.log(new StudentClass('John', 'Doe').workOnHomework());
